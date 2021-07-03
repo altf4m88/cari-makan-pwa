822188d9ad15297b4825a75392b4ed49991ad3be
altf4m88 <fadhilmauladhani@gmail.com>
2021-07-03 19:13:33 +0700

    add empty page for favorite restaurant

--- src/scripts/views/pages/favourite.js
+++ src/scripts/views/pages/favourite.js
@@ -11,13 +11,22 @@ const Favourite = {
               <section id="favorite-restaurants" class="card-container">
                   
               </section>
+              <div class="empty-page">
+                  
+              </div>
           </div>
       `;
   },
 
   async afterRender() {
-    const restaurants = await FavoriteRestaurantIdb.getAllRestaurants();
+    const restaurants = await FavoriteRestaurantIdb.getAllRestaurant();
     const restaurantsContainer = document.querySelector('#favorite-restaurants');
+    const emptyContainer = document.querySelector('.empty-page');
+    console.log(restaurants);
+    if(restaurants.length < 1) {
+      emptyContainer.innerHTML += "<h3 class='no-favorite'>Belum ada restoran yang disukai</h3>";
+    }
+
     restaurants.forEach((restaurant) => {
       restaurantsContainer.innerHTML += createRestaurantItemTemplate(restaurant);
     });
