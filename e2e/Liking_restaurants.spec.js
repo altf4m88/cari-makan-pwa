62844ee6ef5bdb85dbe7316a604d65ad0771e199
altf4m88 <fadhilmauladhani@gmail.com>
2021-07-03 19:43:11 +0700

    add test scenario for unliking a restaurant

--- e2e/Liking_restaurants.spec.js
+++ e2e/Liking_restaurants.spec.js
@@ -28,6 +28,33 @@ Scenario('liking one restaurant', async ({ I }) => {
   I.amOnPage('/#/favourite');
   I.seeElement('.card');
   const likedRestaurantName = await I.grabTextFrom('.card-title');
- 
+
   assert.strictEqual(firstRestaurantName, likedRestaurantName);
+});
+
+Scenario('unliking one restaurant', async ({ I }) => {
+  I.see('Belum ada restoran yang disukai', '.no-favorite');
+
+  I.amOnPage('/');
+
+  I.seeElement('.card-title');
+
+  const firstRestaurant = locate('.card-title').first();
+  I.click(firstRestaurant);
+
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
+
+  I.amOnPage('/#/favourite');
+  I.seeElement('.card');
+
+  const firstFavoriteRestaurant = locate('.card-title').first();
+  I.click(firstFavoriteRestaurant);
+
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
+
+  I.amOnPage('/#/favourite');
+  I.see('Belum ada restoran yang disukai', '.no-favorite');
+
 });
